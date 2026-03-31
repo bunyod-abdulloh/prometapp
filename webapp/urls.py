6cdf9284b35e6b153d40d57a51e73d9a88e059b7
bunyod-abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2026-03-31 10:46:51 +0500

    Commited

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from coreapp.views import home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("main/", include("coreapp.urls"), ),
+    path('', home, name="home"),
     path('api/roof/', include('roofing.urls')),
 ]
 
